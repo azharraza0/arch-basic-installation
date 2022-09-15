71d64d5b61640cfb1e19a5c38718e91c7f966b15
mdazzyy <113507160+mdazzyy@users.noreply.github.com>
2022-09-15 15:58:10 +0530

    Create gnome.sh

---
+++ gnome.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo pacman -Syyu
+
+sudo pacman -S xf86-video-intel gdm eog file-roller gnome-calculator gnome-control-center gnome-disk-utility gnome-keyring gnome-nettool gnome-power-manager gnome-screenshot gnome-shell gnome-terminal gnome-themes-extra gnome-tweaks gvfs gvfs-mtp nautilus xdg-user-dirs-gtk xdg-utils gnome-bluetooth ttf-hack ttf-jetbrains-mono ttf-fira-code ttf-fira-mono power-profiles-daemon gnome-characters noto-fonts-emoji
+
+sudo gpasswd -a mdazzyy rfkill
+
+sudo systemctl enable bluetooth
+
+sudo systemctl enable gdm
