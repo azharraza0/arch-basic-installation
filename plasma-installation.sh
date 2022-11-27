d7b1eb24bf19918d62cf82a253fa65c911591da7
mdazzyy <113507160+mdazzyy@users.noreply.github.com>
2022-11-27 17:28:59 +0530

    Update plasma-installation.sh

--- plasma-installation.sh
+++ plasma-installation.sh
@@ -6,6 +6,6 @@
 #sudo reflector --verbose --country 'India' -l 5 --sort rate --save /etc/pacman.d/mirrorlist
 #sudo pacman -Syyy
 
-sudo pacman -S xf86-video-intel xorg sddm plasma-desktop ark breeze-gtk dolphin gwenview kde-gtk-config khotkeys kinfocenter kinit kio-fuse konsole kscreen plasma-disks plasma-nm plasma-pa kdeplasma-addons powerdevil bluedevil print-manager sddm-kcm xsettingsd xdg-user-dirs xdg-utils gvfs gvfs-mtp ttf-hack noto-fonts-emoji pavucontrol ttf-indic-otf
+sudo pacman -S xf86-video-intel xorg sddm plasma-desktop ark breeze-gtk dolphin gwenview kde-gtk-config khotkeys kinfocenter kinit kio-fuse konsole kscreen plasma-disks plasma-nm plasma-pa kdeplasma-addons powerdevil bluedevil print-manager sddm-kcm xsettingsd xdg-user-dirs xdg-utils gvfs gvfs-mtp ttf-hack noto-fonts-emoji pavucontrol ttf-indic-otf ttf-jetbrains-mono
 
 sudo systemctl enable sddm
