e6e1b3ba53c4b73bc46c39f757c794291289a95f
mdazzyy <113507160+mdazzyy@users.noreply.github.com>
2022-11-28 17:42:13 +0530

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -Syyu
 
-sudo pacman -S xf86-video-intel gdm eog file-roller gnome-calculator gnome-control-center gnome-disk-utility gnome-keyring gnome-nettool gnome-power-manager gnome-screenshot gnome-shell gnome-terminal gnome-themes-extra gnome-tweaks gvfs gvfs-mtp nautilus xdg-user-dirs-gtk xdg-utils gnome-bluetooth ttf-hack ttf-jetbrains-mono ttf-fira-code ttf-fira-mono power-profiles-daemon gnome-characters noto-fonts-emoji pavucontrol
+sudo pacman -S xf86-video-intel gdm eog file-roller gnome-calculator gnome-control-center gnome-disk-utility gnome-keyring gnome-nettool gnome-power-manager gnome-screenshot gnome-shell gnome-terminal gnome-themes-extra gnome-tweaks gvfs gvfs-mtp nautilus xdg-user-dirs-gtk xdg-utils gnome-bluetooth ttf-indic-otf ttf-hack ttf-jetbrains-mono ttf-fira-code ttf-fira-mono power-profiles-daemon gnome-characters noto-fonts-emoji pavucontrol
 
 sudo gpasswd -a mdazzyy rfkill
 
