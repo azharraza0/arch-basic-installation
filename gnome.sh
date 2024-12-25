ac24179eb4060c7579bd2e5959b47ffd0d32d254
Azhar <113507160+azharraza0@users.noreply.github.com>
2024-12-25 22:11:02 +0530

    add "sof-firmware" package

--- gnome.sh
+++ gnome.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -Syyu
 
-sudo pacman -S xf86-video-intel gdm eog file-roller gnome-calculator gnome-control-center gnome-disk-utility gnome-font-viewer gnome-keyring gnome-nettool gnome-power-manager gnome-screenshot gnome-shell gnome-terminal gnome-text-editor gnome-themes-extra gnome-tweaks gvfs gvfs-mtp nautilus xdg-user-dirs-gtk xdg-utils gnome-bluetooth ttf-indic-otf ttf-hack ttf-jetbrains-mono ttf-fira-code ttf-fira-mono power-profiles-daemon gnome-characters noto-fonts-emoji pavucontrol
+sudo pacman -S xf86-video-intel gdm eog file-roller gnome-calculator gnome-control-center gnome-disk-utility gnome-font-viewer gnome-keyring gnome-nettool gnome-power-manager gnome-screenshot gnome-shell gnome-terminal gnome-text-editor gnome-themes-extra gnome-tweaks gvfs gvfs-mtp nautilus xdg-user-dirs-gtk xdg-utils gnome-bluetooth ttf-indic-otf ttf-hack ttf-jetbrains-mono ttf-fira-code ttf-fira-mono power-profiles-daemon gnome-characters noto-fonts-emoji pavucontrol sof-firmware
 
 sudo gpasswd -a mdazzyy rfkill
 
